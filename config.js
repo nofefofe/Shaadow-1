5b7c3c4414088de061ce56f959b63ef869d3c714
Glitch (f421) <none>
2020-03-07 14:40:51 +0000

    🌔📉 Checkpoint

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "Njg1MjA3MzMyOTI5Nzk4MTgx.XmFTQQ.iyuDquRBd_MBS59qZt5J4p4ZUIA", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "%",
+  devs: ["684786589444210708"] ///تعديل مهم ايدي حسابك
+};
