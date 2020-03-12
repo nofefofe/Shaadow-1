a4f28fbf95475bae7044cd80d48863478e171256
Glitch (f421) <none>
2020-03-12 16:42:14 +0000

    🏰🐟 Checkpoint ./config.js:12587828/118

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "Njg1MjA3MzMyOTI5Nzk4MTgx.XmkoGg.QqowTk2Qzmqt6TAla925vX3QsRo", //تعديل مهم توكن بوتك
+  TOKEN: "Njg1MjA3MzMyOTI5Nzk4MTgx.Xmpj3A.BzYcNtulLQ6Dq_B-yDBzgiznqd8", //تعديل مهم توكن بوتك
   YT_API_KEY: "api ",
   prefix: "%",
   devs: ["528954929453203456"] ///تعديل مهم ايدي حسابك
