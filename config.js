51a48c2593d637eaca8b4f08c2c111133ddf37c6
Glitch (f421) <none>
2020-03-20 17:05:22 +0000

    🍀🌝 Checkpoint ./config.js:8374370/234 ./server.js:8374370/590

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   TOKEN: "NjkwNjAzNjgwMTc2NTM3NjIx.XnT00Q.S9nmyTUIvkaXGLq73iJmCrPMzk0", //تعديل مهم توكن بوتك
   YT_API_KEY: "api ",
   prefix: "=",
-  devs: ["ايدي حسابك"] ///تعديل مهم ايدي حسابك
+  devs: ["335645388323160064"] ///
 };
